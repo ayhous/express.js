903015499fb637c6d64fc5f79b3342fd000e243b
ayhous <ayouzi86@gmail.com>
2023-08-18 12:30:26 +0200

    tester liste citation url

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -2,6 +2,7 @@ const {Router}  =require('express');
 
 const userRouter=Router();
 
+
 userRouter.get('/users',(request,response) => {
     response.send({
         users:[
@@ -12,5 +13,50 @@ userRouter.get('/users',(request,response) => {
         ]
     })
 })
+userRouter.get('/citation',(request,response) => {
 
+    response.send({
+        citations:
+        [{
+            "_id": {
+              "$oid": "64dcba671df43b43a93f3e03"
+            },
+            "text": "La beauté réside dans les yeux de celui qui contemple, car chaque regard est une interprétation unique."
+          },
+          {
+            "_id": {
+              "$oid": "64dcdac119e848e7157d6cbe"
+            },
+            "text": "hshshs"
+          },
+          {
+            "_id": {
+              "$oid": "64dcdb6a19e848e7157d6cbf"
+            },
+            "text": "je suis la"
+          },
+          {
+            "_id": {
+              "$oid": "64dcdbe1c591da35d77e6ea7"
+            },
+            "text": "ooooooooo"
+          },
+          {
+            "_id": {
+              "$oid": "64dcdca17c1b521d2d6d9aed"
+            },
+            "text": "houssin"
+          },
+          {
+            "_id": {
+              "$oid": "64dcde2c7c1b521d2d6d9aee"
+            },
+            "text": "rever c est realiser"
+          }]
+    })
+
+})
+userRouter.get('/version',(request,response) => {
+    response.send('je suis la version x')
+})
 module.exports=userRouter;
--- express-init.js
+++ express-init.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const userRouter=require('./Routers/user.router.js')
 
+
+const expressVersion = express.version;
+console.log('Version d\'Express.js :', expressVersion);
+
+const apiVersion = 'v1';
+
 const app=express();
 
 app.get('/hello', (req, res)=>{
@@ -14,3 +20,8 @@ app.use(userRouter);
 app.listen(8001,()=>{
     console.log("pret a l ecout notre serveur")
 })
+
+/* Créez la base de ce qui servira pour votre Bot
+- Créez un server express
+- Créez une route /citations qui renverra la liste des citations
+- une route /version qui renverra la version de votre API */
